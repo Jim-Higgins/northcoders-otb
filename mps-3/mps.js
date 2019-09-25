02b13102ff41c4d206f5d31555a6a40af6335305
Jim Higgins <Jim.Higgins@onthebeach.co.uk>
2019-09-25 12:11:33 +0100

    firstname and lastname added to return object

--- mps-3/mps.js
+++ mps-3/mps.js
@@ -1,8 +1,14 @@
 function addFirstAndLastNames(input) {
   const result = [];
-  if (input[0]) {
-    result.push({...input[0]});
+  if (!input[0]) return [];
+
+  if (input[0].name) {
+    let [firstName, lastName] = input[0].name.split(' ');
+    result.push({ ...input[0], firstName, lastName });
+  } else {
+    result.push({ ...input[0] });
   }
+
   return result;
 }
 module.exports = { addFirstAndLastNames };
--- mps-3/mps.test.js
+++ mps-3/mps.test.js
@@ -1,24 +1,65 @@
-const {addFirstAndLastNames} = require('./mps')
+const { addFirstAndLastNames } = require('./mps');
 
 describe('MP utils', () => {
   describe('addFirstAndLastNames()', () => {
     test('should return an empty array', () => {
-      expect(addFirstAndLastNames([])).toEqual([])
+      expect(addFirstAndLastNames([])).toEqual([]);
     });
 
     it('should return a new array', () => {
-      let input = []
-      expect(addFirstAndLastNames(input)).not.toBe(input)
+      let input = [];
+      expect(addFirstAndLastNames(input)).not.toBe(input);
     });
 
     it('should return an empty object when given an empty object', () => {
-      let input = [{}]
-      expect(addFirstAndLastNames(input)).toEqual([{}])
+      let input = [{}];
+      expect(addFirstAndLastNames(input)).toEqual([{}]);
     });
 
     it('should return a new object', () => {
-      let input = [{}]
-      expect(addFirstAndLastNames(input)[0]).not.toBe(input[0])
+      let input = [{}];
+      expect(addFirstAndLastNames(input)[0]).not.toBe(input[0]);
     });
+
+    it('should return first name and last name to the object', () => {
+      let input = [
+        {
+          member_id: '41899',
+          person_id: '25227',
+          name: 'Robert Jenrick',
+          party: 'Conservative',
+          constituency: 'Newark',
+          office: [
+            {
+              dept: '',
+              position: 'The Exchequer Secretary',
+              from_date: '2018-01-09',
+              to_date: '9999-12-31',
+            },
+          ],
+        },
+      ];
+      expect(addFirstAndLastNames(input)).toEqual([
+        {
+          member_id: '41899',
+          person_id: '25227',
+          name: 'Robert Jenrick',
+          firstName : 'Robert',
+          lastName : 'Jenrick',
+          party: 'Conservative',
+          constituency: 'Newark',
+          office: [
+            {
+              dept: '',
+              position: 'The Exchequer Secretary',
+              from_date: '2018-01-09',
+              to_date: '9999-12-31',
+            },
+          ],
+        },
+      ]);
+    });
+
+
   });
 });
