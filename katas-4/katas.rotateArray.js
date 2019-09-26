b9ef730962c460a7718bb69b0caf0774fed0424e
Jim Higgins <Jim.Higgins@onthebeach.co.uk>
2019-09-26 09:41:33 +0100

    a new array should be returned

--- katas-4/katas.rotateArray.js
+++ katas-4/katas.rotateArray.js
@@ -1,4 +1,4 @@
-function rotateArray() {
+function rotateArray(input) {
     return []
 }
 
--- katas-4/katas.rotateArray.test.js
+++ katas-4/katas.rotateArray.test.js
@@ -2,7 +2,11 @@ const { rotateArray } = require('./katas.rotateArray')
 
 describe('rotateArray()', () => {
     it('should return an empty array', () => {
-        expect(rotateArray()).toEqual([])
+        expect(rotateArray([])).toEqual([])
     });
     
+    it('should return a new array', () => {
+        let input = []
+        expect(rotateArray(input)).not.toBe(input)
+    });
 });
