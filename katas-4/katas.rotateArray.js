bb923aa479b96a7eb6e212af0ef58aa0b05b8c2a
Jim Higgins <Jim.Higgins@onthebeach.co.uk>
2019-09-30 08:54:22 +0100

    test for rotateBy 1 number given

--- katas-4/katas.rotateArray.js
+++ katas-4/katas.rotateArray.js
@@ -1,10 +1,20 @@
-function rotateArray(inputArray) {
+function rotateArray(inputArray, rotateBy) {
+  const newInputArray = [...inputArray];
+  if (!inputArray) {
+    return [];
+  }
+  else{
+  //splice out the last number in the array, bt the rotateBy number.
+  let removed = newInputArray.splice(-1, rotateBy);
+  let inserted = removed.concat(newInputArray);
+  
+  console.log(inserted);
+  return inserted;
+  }
 
-    const newInputArray = [...inputArray]
 
-    if(!inputArray) return []
 
-    return newInputArray
+ 
 }
 
 module.exports = { rotateArray };
--- katas-4/katas.rotateArray.test.js
+++ katas-4/katas.rotateArray.test.js
@@ -1,6 +1,6 @@
 const { rotateArray } = require('./katas.rotateArray')
 
-describe('rotateArray()', () => {
+describe.only('rotateArray()', () => {
     it('should return an empty array', () => {
         expect(rotateArray([])).toEqual([])
     });
@@ -12,7 +12,13 @@ describe('rotateArray()', () => {
 
     it('should return a unchanged array when given no number', () => {
         let inputArray = [1, 2, 3]
+        // let rotateBy = 0
         expect(rotateArray(inputArray)).toEqual([1, 2, 3])
-        
+    });
+
+    it('should rotate the array by 1 to the right', () => {
+        let inputArray = [1, 2, 3]
+        let rotateBy = 1
+        expect(rotateArray(inputArray, rotateBy)).toEqual([3, 1, 2])
     });
 });
